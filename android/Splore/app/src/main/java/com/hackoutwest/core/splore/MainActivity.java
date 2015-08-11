9cd8c3df83ef3936f06744ed5b4f1d79ba93d108
Andreas Rolén <andreasrolen93@gmail.com>
2015-08-12 01:35:13 +0200

    Fixed the connection to the server

--- android/Splore/app/src/main/java/com/hackoutwest/core/splore/MainActivity.java
+++ android/Splore/app/src/main/java/com/hackoutwest/core/splore/MainActivity.java
@@ -23,6 +23,16 @@ import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -31,6 +41,8 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.transform.Result;
 
@@ -47,6 +59,8 @@ public class MainActivity extends Activity implements
     private GoogleApiClient mGoogleApiClient;
 
     private WebView mWebView;
+
+    private boolean isAuthed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,19 +91,16 @@ public class MainActivity extends Activity implements
         };
         registerReceiver(receiver, new IntentFilter("com.hackoutwest.broadcast.gps.location_change"));
 
-        Intent servIntent = new Intent(this, SploreLocationService.class);
-        startService(servIntent);
+      //  Intent servIntent = new Intent(this, SploreLocationService.class);
+       // startService(servIntent);
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
         mWebView.setWebViewClient(new SploreWebViewClient());
         mWebView.addJavascriptInterface(new SploreWebInterface(this), "Android");
-        mWebView.loadUrl("http://10.47.12.93:3000");
+        mWebView.loadUrl("http://10.47.12.57:3000");
 
-    }
-
-    private String locationToJSON(double lat, double lon) {
-        return "{'location':{'lat':"+lat+",'long':"+lon+"}}";
     }
 
 
@@ -124,7 +135,7 @@ public class MainActivity extends Activity implements
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         // Check if the key event was the Back button and if there's history
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack() && !isAuthed) {
             mWebView.goBack();
             return true;
         }
@@ -144,6 +155,7 @@ public class MainActivity extends Activity implements
 
         @JavascriptInterface
         public String getID() {
+            Log.d("Servi", "Get ID");
             SharedPreferences settings = mContext.getSharedPreferences("PREFS_NAME", 0);
             return settings.getString("USER_ID","-1");
         }
@@ -155,8 +167,15 @@ public class MainActivity extends Activity implements
 
         @JavascriptInterface
         public void startLocationService() {
+            isAuthed = true;
             Intent servIntent = new Intent(mContext, SploreLocationService.class);
             startService(servIntent);
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -165,11 +184,30 @@ public class MainActivity extends Activity implements
         @Override
         protected Result doInBackground(Double... params) {
 
-            String jsonLocation = locationToJSON(params[0], params[1]);
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://10.47.12.57:3000/update");
+
+            SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
+            String userID = settings.getString("USER_ID", "-1");
 
             try {
-                //url = new URL("http://10.47.12.93:3000/update");
-                URL url = new URL("http://www.google.com");
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+                nameValuePairs.add(new BasicNameValuePair("userID", userID));
+                nameValuePairs.add(new BasicNameValuePair("latitude", params[0]+""));
+                nameValuePairs.add(new BasicNameValuePair("longitude", params[1]+""));
+
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            }
+            /*
+            try {
+                URL url = new URL("http://10.47.12.57:3000/update");
                 SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
                 String userID = settings.getString("USER_ID", "-1");
 
@@ -181,14 +219,18 @@ public class MainActivity extends Activity implements
                 conn.setDoOutput(true);
 
                 StringBuilder result = new StringBuilder();
-                result.append(URLEncoder.encode("userID", "UTF-8"));
+                result.append(URLEncoder.encode("phoneID", "UTF-8"));
                 result.append("=");
                 result.append(URLEncoder.encode(userID, "UTF-8"));
                 result.append("&");
-                result.append(URLEncoder.encode("location", "UTF-8"));
+                result.append(URLEncoder.encode("latitude", "UTF-8"));
                 result.append("=");
-                result.append(URLEncoder.encode(jsonLocation, "UTF-8"));
-
+                result.append(URLEncoder.encode(params[0]+"", "UTF-8"));
+                result.append("&");
+                result.append(URLEncoder.encode("longitude", "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(params[1]+"", "UTF-8"));
+                Log.d("Result", result.toString());
                 OutputStream os = conn.getOutputStream();
                 BufferedWriter writer = new BufferedWriter(
                         new OutputStreamWriter(os, "UTF-8"));
@@ -203,6 +245,7 @@ public class MainActivity extends Activity implements
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            */
             return null;
         }
 
